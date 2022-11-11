fb10ed8a172e9c724f18893aa506f2cc459c2bac
Redstonerayy <redstone.rayy@gmail.com>
2022-11-11 12:09:29 +0100

    download and upload files

--- iserv/files.go
+++ iserv/files.go
@@ -2,6 +2,7 @@ package iserv
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 
 	"github.com/studio-b12/gowebdav"
@@ -21,4 +22,32 @@ func (c *IServFilesClient) Login(config *IServAccountConfig) error {
 
 func (c *IServFilesClient) ReadDir(path string) ([]os.FileInfo, error) {
 	return c.DavClient.ReadDir(path)
+}
+
+func (c *IServFilesClient) DownloadFile(webdavpath string, localpath string) error {
+	bytes, err := c.DavClient.Read(webdavpath)
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(localpath, bytes, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *IServFilesClient) UploadFile(webdavpath string, localpath string) error {
+	bytes, err := ioutil.ReadFile(localpath)
+	if err != nil {
+		return err
+	}
+
+	err = c.DavClient.Write(webdavpath, bytes, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
