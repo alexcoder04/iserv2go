9ee83ec38886e8732bb84381fe8f98803e5f2fca
alexcoder04 <alexcoder04@protonmail.com>
2022-11-11 18:12:32 +0100

    clean up, add more file functions

--- iserv/files.go
+++ iserv/files.go
@@ -24,30 +24,28 @@ func (c *IServFilesClient) ReadDir(path string) ([]os.FileInfo, error) {
 	return c.DavClient.ReadDir(path)
 }
 
-func (c *IServFilesClient) DownloadFile(webdavpath string, localpath string) error {
-	bytes, err := c.DavClient.Read(webdavpath)
-	if err != nil {
-		return err
-	}
-
-	err = ioutil.WriteFile(localpath, bytes, 0644)
-	if err != nil {
-		return err
-	}
-
-	return nil
+func (c *IServFilesClient) ReadFile(path string) ([]byte, error) {
+	return c.DavClient.Read(path)
 }
 
-func (c *IServFilesClient) UploadFile(webdavpath string, localpath string) error {
+func (c *IServFilesClient) DownloadFile(webdavpath string, localpath string) error {
+	bytes, err := c.ReadFile(webdavpath)
+	if err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile(localpath, bytes, 0600)
+}
+
+func (c *IServFilesClient) WriteFile(path string, content []byte) error {
+	return c.DavClient.Write(path, content, 0600)
+}
+
+func (c *IServFilesClient) UploadFile(localpath string, webdavpath string) error {
 	bytes, err := ioutil.ReadFile(localpath)
 	if err != nil {
 		return err
 	}
 
-	err = c.DavClient.Write(webdavpath, bytes, 0644)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return c.WriteFile(webdavpath, bytes)
 }
