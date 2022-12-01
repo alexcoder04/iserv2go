6ab488c3b88281bd9d7c9db9de432e8fee0edc7d
alexcoder04 <alexcoder04@protonmail.com>
2022-12-01 15:29:19 +0100

    finish exercises

--- iserv/web/exercise.go
+++ iserv/web/exercise.go
@@ -1,61 +1,12 @@
 package web
 
 import (
-	"time"
 	"sync"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/alexcoder04/iserv2go/iserv/types"
 )
-
-func getExerciseInfo(url string) (types.IServExercise, error) {
-	return types.IServExercise{}, nil
-}
-
-func (c *IServWebClient) getExercisesFrom(path string) ([]types.IServExercise, error) {
-	// get html page of exercise overview
-	doc, err := c.doGetRequestQueryDoc("/exercise" + path)
-	if err != nil {
-		return []types.IServExercise{}, err
-	}
-
-	// for each task get the url
-	var urls []string
-	doc.Find("#crud-table tbody tr").Each(func(i int, s *goquery.Selection) {
-		if !s.HasClass("info") {
-			url, _ := s.Children().Eq(1).Children().First().Attr("href")
-			urls = append(urls, url)
-		}
-	})
-
-
-	// get info for all exercises concurrently
-	exercises := []types.IServExercise{}
-
-	var wg sync.WaitGroup
-	var mu sync.Mutex
-
-	for _, url := range urls {
-		wg.Add(1)
-
-		go func(url string, wg *sync.WaitGroup) {
-			exercise, err := getExerciseInfo(url)
-			if err != nil {
-				return
-			}
-
-			mu.Lock()
-			exercises = append(exercises, exercise)
-			mu.Unlock()
-
-			wg.Done()
-		}(url, &wg)
-	}
-
-	wg.Wait()
-
-	return exercises, nil
-}
 
 func (c *IServWebClient) getExerciseInfo(url string) (types.IServExercise, error) {
 	// get html page of specific exercise
@@ -110,20 +61,49 @@ func (c *IServWebClient) getExerciseInfo(url string) (types.IServExercise, error
 	})
 	exercise.Files = fileurls
 
-	// print out
-	// fmt.Println(title)
-	// fmt.Println(duedate)
-	// fmt.Println(startdate)
-	// fmt.Println(teacher)
-	// fmt.Println("Fileurls:")
-	// for _, fileurl := range fileurls {
-	// 	fmt.Println(fileurl)
-	// }
-	// fmt.Println("Types:")
-	// for _, submittype := range types {
-	// 	fmt.Println(submittype)
-	// }
-	// fmt.Println(description)
-
 	return exercise, nil
+}
+
+func (c *IServWebClient) getExercisesFrom(path string) ([]types.IServExercise, error) {
+	// get html page of exercise overview
+	doc, err := c.doGetRequestQueryDoc("/exercise" + path)
+	if err != nil {
+		return []types.IServExercise{}, err
+	}
+
+	// for each task get the url
+	var urls []string
+	doc.Find("#crud-table tbody tr").Each(func(i int, s *goquery.Selection) {
+		if !s.HasClass("info") {
+			url, _ := s.Children().Eq(1).Children().First().Attr("href")
+			urls = append(urls, url)
+		}
+	})
+
+	// get info for all exercises concurrently
+	exercises := []types.IServExercise{}
+
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+
+	for _, url := range urls {
+		wg.Add(1)
+
+		go func(url string, wg *sync.WaitGroup) {
+			exercise, err := c.getExerciseInfo(url)
+			if err != nil {
+				return
+			}
+
+			mu.Lock()
+			exercises = append(exercises, exercise)
+			mu.Unlock()
+
+			wg.Done()
+		}(url, &wg)
+	}
+
+	wg.Wait()
+
+	return exercises, nil
 }
