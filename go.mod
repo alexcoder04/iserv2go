6a7470bf222edcfe445d3f8a1b99da04a36ca0fa
alexcoder04 <alexcoder04@protonmail.com>
2022-12-25 12:28:45 +0100

    use friendly for getCacheDir

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.18
 
 require (
 	github.com/PuerkitoBio/goquery v1.8.0
-	github.com/alexcoder04/friendly/v2 v2.1.0
+	github.com/alexcoder04/friendly/v2 v2.1.1
 	github.com/emersion/go-imap v1.2.1
 	github.com/joho/godotenv v1.4.0
 	github.com/studio-b12/gowebdav v0.0.0-20221102155456-200a600c0272
@@ -14,5 +14,7 @@ require (
 require (
 	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/emersion/go-sasl v0.0.0-20200509203442-7bfe0ed36a21 // indirect
+	github.com/otiai10/copy v1.9.0 // indirect
+	golang.org/x/sys v0.3.0 // indirect
 	golang.org/x/text v0.5.0 // indirect
 )
--- iserv/web/auth.go
+++ iserv/web/auth.go
@@ -5,30 +5,10 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
-	"os"
 	"path"
+
+	"github.com/alexcoder04/friendly/v2/ffiles"
 )
-
-// TODO use the friendly implmentation
-func GetCacheFolder() string {
-	dir, err := os.UserCacheDir()
-	if err != nil {
-		home, err := os.UserHomeDir()
-		if err != nil {
-			return "."
-		}
-		dir = path.Join(home, ".cache")
-	}
-
-	ourDir := path.Join(dir, "iserv2go")
-
-	err = os.MkdirAll(ourDir, 0700)
-	if err != nil {
-		return "."
-	}
-
-	return ourDir
-}
 
 func (c *WebClient) saveCredentials() error {
 	url, err := url.Parse(c.config.IServUrl())
@@ -41,11 +21,21 @@ func (c *WebClient) saveCredentials() error {
 		return err
 	}
 
-	return ioutil.WriteFile(path.Join(GetCacheFolder(), "cookies.json"), data, 0600)
+	dir, err := ffiles.GetCacheDirFor("iserv2go")
+	if err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile(path.Join(dir, "cookies.json"), data, 0600)
 }
 
 func (c *WebClient) loadCredentials() error {
-	data, err := ioutil.ReadFile(path.Join(GetCacheFolder(), "cookies.json"))
+	dir, err := ffiles.GetCacheDirFor("iserv2go")
+	if err != nil {
+		return err
+	}
+
+	data, err := ioutil.ReadFile(path.Join(dir, "cookies.json"))
 	if err != nil {
 		return err
 	}
